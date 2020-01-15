4f0d210915d656eb33aa02392318264e49ca88b2
S. Andrew Sheppard <andrew@wq.io>
2020-01-15 11:26:56 +0900

    update python version

--- setup.py
+++ setup.py
@@ -68,6 +68,7 @@ setup(
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
         'Topic :: Software Development :: Libraries :: Application Frameworks',
         'Topic :: Software Development :: Build Tools',
     ]
