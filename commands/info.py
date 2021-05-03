1f0fde1dc4619dbd83a71e735aa885cdb9891b9d
S. Andrew Sheppard <andrew@wq.io>
2021-05-03 11:56:35 +0900

    update to match new docs structure

--- commands/info.py
+++ commands/info.py
@@ -43,24 +43,22 @@ def print_versions(output, libraries=[]):
             click.echo(dep)
 
 
-DOC_LAYOUT = """---
-order: {order}
----
-
-wq {name}
+DOC_LAYOUT = """wq {name}
 {title_line}
 
 wq {name}: {short_help}
-Provided by [wq.{mod}](https://wq.io/wq.{mod}).
+Provided by [{mod}].
 
 ```shell
 $ wq {name} --help
 
 {help}
-```"""
+```
+
+[{mod}]: ./index.md"""
 
 INDEX_LAYOUT = """---
-order: 0
+order: -1
 ---
 
 wq
@@ -79,43 +77,49 @@ $ wq --help
 
 Command | Module | Description
 --------|--------|-------------"""
-INDEX_ROW = (
-    "[{name}](https://wq.io/docs/wq-{name})"
-    " | [wq.{mod}](https://wq.io/wq.{mod})"
-    " | {short_help}"
-)
+INDEX_ROW = "[{name}] | [{mod}] | {short_help}"
+LINK_ROW = "[{name}]: ../{mod}/{name}.md"
 
 
 @wq.command()
 @click.pass_context
 def _make_docs(ctx):
     command_list = []
+    modules = set()
     for i, (name, cmd) in enumerate(sorted(wq.commands.items())):
         if name.startswith('_'):
             continue
+        if 'DEPRECATED' in cmd.short_help:
+            continue
 
-        with open('wq-%s.md' % name, 'w') as f:
-            module_path = cmd.callback.__module__.split('.')
-            if module_path[0] != 'wq':
-                continue
+        mod = '.'.join(cmd.callback.__module__.split('.')[:2])
+        if not mod.startswith('wq.'):
+            continue
 
+        modules.add(mod)
+
+        with open('%s/%s.md' % (mod, name), 'w') as f:
             cctx = click.Context(cmd, info_name=name, parent=ctx.parent)
             command_info = dict(
                 name=name,
-                order=i+1,
                 title_line='=' * (len(name) + 3),
                 short_help=cmd.short_help,
-                mod=module_path[1],
+                mod=mod,
                 help=cmd.get_help(cctx),
             )
             print(DOC_LAYOUT.format(**command_info), file=f)
             command_list.append(command_info)
 
     wq_help = wq.get_help(ctx.parent).split('Commands:')[0].strip()
-    with open('wq.md', 'w') as f:
+    with open('wq.build/cli.md', 'w') as f:
         print(INDEX_LAYOUT.format(
            short_help=wq.short_help,
            help=wq_help
         ), file=f)
         for command_info in command_list:
             print(INDEX_ROW.format(**command_info), file=f)
+        print("", file=f)
+        for mod in sorted(modules):
+            print("[{mod}]: ../{mod}/index.md".format(mod=mod), file=f)
+        for command_info in command_list:
+            print(LINK_ROW.format(**command_info), file=f)
